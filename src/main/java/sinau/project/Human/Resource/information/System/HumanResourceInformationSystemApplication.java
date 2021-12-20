1e4ad65ed81cecadf1e2daede2b80382ff040874
bkeyunknown <kikizan01@gmail.com>
2021-12-20 11:44:39 +0700

    first commit

---
+++ src/main/java/sinau/project/Human/Resource/information/System/HumanResourceInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package sinau.project.Human.Resource.information.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceInformationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sinau/project/Human/Resource/information/System/HumanResourceInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sinau.project.Human.Resource.information.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
