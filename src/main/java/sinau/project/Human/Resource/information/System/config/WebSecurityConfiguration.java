b23ba8a77fe9e0706238f4849362d34eb129ecf4
bkeyunknown <kikizan01@gmail.com>
2021-12-21 16:25:43 +0700

    update path permission

--- src/main/java/sinau/project/Human/Resource/information/System/config/WebSecurityConfiguration.java
+++ src/main/java/sinau/project/Human/Resource/information/System/config/WebSecurityConfiguration.java
@@ -29,7 +29,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll()
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
                 .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
