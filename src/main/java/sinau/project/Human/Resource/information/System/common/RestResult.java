42c79b44dd0a07094dce5295012869dcdf2ba469
bkeyunknown <kikizan01@gmail.com>
2021-12-21 09:46:29 +0700

    update RestResult

--- src/main/java/sinau/project/Human/Resource/information/System/common/RestResult.java
+++ src/main/java/sinau/project/Human/Resource/information/System/common/RestResult.java
@@ -1,4 +1,23 @@
 package sinau.project.Human.Resource.information.System.common;
 
-public class RestResult {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RestResult implements Serializable {
+
+    private Object data;
+
+    private Long rows;
+
+    private String status = StatusCode.OPERATION_SUCCESS;
+
 }
