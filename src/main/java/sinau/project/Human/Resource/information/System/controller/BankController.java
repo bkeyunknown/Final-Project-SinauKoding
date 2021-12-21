32f7b74007ec8ba0f23cd3805eec9a6df75dfebb
bkeyunknown <kikizan01@gmail.com>
2021-12-21 11:05:00 +0700

    update CRUD BankController

--- src/main/java/sinau/project/Human/Resource/information/System/controller/BankController.java
+++ src/main/java/sinau/project/Human/Resource/information/System/controller/BankController.java
@@ -1,13 +1,16 @@
 package sinau.project.Human.Resource.information.System.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sinau.project.Human.Resource.information.System.common.RestResult;
+import sinau.project.Human.Resource.information.System.common.StatusCode;
 import sinau.project.Human.Resource.information.System.entity.Bank;
 import sinau.project.Human.Resource.information.System.service.BankService;
+
+import java.util.ArrayList;
 
 @RestController
 @PreAuthorize("permitAll()")
@@ -17,9 +20,33 @@ public class BankController extends BaseController {
     @Autowired
     private BankService service;
 
+    @GetMapping
+    public RestResult get(@RequestParam(value = "param", required = false) String param,
+                          @RequestParam(value = "offset") int offset,
+                          @RequestParam(value = "limit") int limit) throws JsonProcessingException {
+
+        Bank bank = param != null ? new ObjectMapper().readValue(param, Bank.class) : null;
+        Long rows = service.count(bank);
+
+        return new RestResult(rows > 0 ? service.find(bank, offset, limit) : new ArrayList<>(), rows);
+
+    }
+
     @PostMapping
-    public Bank save(@RequestBody Bank param) {
-        return service.save(param);
+    public RestResult save(@RequestBody Bank param) {
+        param = service.save(param);
+        return new RestResult(param, param != null ? StatusCode.SAVE_SUCCESS : StatusCode.SAVE_FAILED);
+    }
+
+    @PutMapping
+    public RestResult update(@RequestBody Bank bank) {
+        bank = service.update(bank);
+        return new RestResult(bank, bank != null ? StatusCode.UPDATE_SUCCESS : StatusCode.UPDATE_FAILED);
+    }
+
+    @DeleteMapping(value = "{id}")
+    public RestResult delete(@PathVariable Long id) {
+        return new RestResult(service.delete(id) ? StatusCode.DELETE_SUCCESS : StatusCode.DELETE_FAILED);
     }
 
 }
