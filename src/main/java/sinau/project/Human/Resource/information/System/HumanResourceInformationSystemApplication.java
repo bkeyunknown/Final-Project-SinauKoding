3f3541f8f74e888f0d05c471ffbb2a16e2ff5857
bkeyunknown <kikizan01@gmail.com>
2021-12-22 09:32:20 +0700

    add getCurrentUser

--- src/main/java/sinau/project/Human/Resource/information/System/HumanResourceInformationSystemApplication.java
+++ src/main/java/sinau/project/Human/Resource/information/System/HumanResourceInformationSystemApplication.java
@@ -2,12 +2,26 @@ package sinau.project.Human.Resource.information.System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import sinau.project.Human.Resource.information.System.entity.User;
 
 @SpringBootApplication
 public class HumanResourceInformationSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourceInformationSystemApplication.class, args);
+	}
+
+	public static User getCurrentUser() {
+		try {
+			Object pricipal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+			if (pricipal != null && pricipal.getClass().equals(User.class)) {
+				return (User) pricipal;
+			}
+		} catch (Exception ignore) {}
+
+		return null;
 	}
 
 }
